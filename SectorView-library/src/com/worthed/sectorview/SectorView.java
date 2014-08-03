f0504c212c7147d7f0ed0c5f4c4a0d692b9f714c
jingle1267 <jingle1267@163.com>
2014-08-03 17:58:41 +0800

    Add custom setting.

--- SectorView-library/src/com/worthed/sectorview/SectorView.java
+++ SectorView-library/src/com/worthed/sectorview/SectorView.java
@@ -1,20 +1,28 @@
 package com.worthed.sectorview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.Shader.TileMode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class SectorView extends ImageView {
 
+	private final String TAG = SectorView.class.getSimpleName();
+	
+	private boolean isShowBound = false;
+	
+	private int boundColor;
+	private float boundSize;
+	
 	private Bitmap bitmap;
 
 	private BitmapShader bitmapShader;
@@ -23,27 +31,40 @@ public class SectorView extends ImageView {
 	
 	public SectorView(Context context) {
 		super(context);
-		init();
+	}
+	
+	public SectorView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context, attrs, 0);
 	}
 
 	public SectorView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		init();
+		
+		init(context, attrs, defStyle);
 	}
 
-	public SectorView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
-	}
+	private void init(Context context, AttributeSet attrs, int defStyle) {
 
-	private void init() {
+		if (attrs != null) {
+			TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.SectorView);;
+			isShowBound = array.getBoolean(R.styleable.SectorView_showBound, false);
+			boundColor = array.getColor(R.styleable.SectorView_boundColor, Color.GRAY);
+			boundSize = array.getDimension(R.styleable.SectorView_boundSize, 2);
+			array.recycle();
+			
+			Log.d(TAG, "isShowBound : " + isShowBound);
+			Log.d(TAG, "boundColor : " + boundColor);
+			Log.d(TAG, "boundSize : " + boundSize);
+		}
+		
 		paint = new Paint();
 		paint.setAntiAlias(true);
 		boundPaint = new Paint();
 		boundPaint.setAntiAlias(true);
-		boundPaint.setColor(Color.GRAY);
+		boundPaint.setColor(boundColor);
 		boundPaint.setStyle(Paint.Style.STROKE);
-		boundPaint.setStrokeWidth(5);
+		boundPaint.setStrokeWidth(boundSize);
 	}
 	
 	@Override
@@ -58,6 +79,10 @@ public class SectorView extends ImageView {
 		if (bitmap.getHeight() == 0 || bitmap.getWidth() == 0)
 			return;
 
+		if (bitmapShader == null) {
+			bitmapShader = new BitmapShader(bitmap, TileMode.CLAMP, TileMode.CLAMP);
+		}
+		
 		paint.setShader(bitmapShader);
 		
 		float radius = Math.min(bitmap.getWidth(), bitmap.getHeight()) / 2;
@@ -66,7 +91,9 @@ public class SectorView extends ImageView {
 		canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2, radius, paint);
 		
 		// draw bound
-        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2, radius, boundPaint);
+		if (isShowBound) {
+			canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2, radius, boundPaint);
+		}
 		
 	}
 
